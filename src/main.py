72766fa388617c9b69e24e72d40e7b84c8995409
borja-sc <borja.sc95@gmail.com>
2023-07-09 17:48:04 +0200

    Correct import

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 from torchvision import datasets, transforms
-from FedEnsemble import fed_ensemble
+from FedEnsemble import train
 from models import BaseModel
 
 def main():
