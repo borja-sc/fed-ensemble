2767627f6e971ae593f3ed816e4700a529482fca
borja-sc <borja.sc95@gmail.com>
2023-07-09 17:43:45 +0200

    Bugfixing

--- src/main.py
+++ src/main.py
@@ -4,18 +4,19 @@ import torch.optim as optim
 import torch.utils.data as data
 from torchvision import datasets, transforms
 from FedEnsemble import fed_ensemble
-from models import BaseModel, train_local
+from models import BaseModel
 
 def main():
     # Set Fed-Ensemble hyperparameters
     NUM_MODELS = 5
     NUM_CLIENTS = 100
-    NUM_SELECTED_CLIENTS = 10
-    NUM_AGES = 5
+    NUM_STRATA = 10
+    NUM_SELECTED_CLIENTS = 5
+    NUM_AGES = 50
     batch_size = 64
-    num_epochs = 10
+    num_epochs = 2
     learning_rate = 0.001
-    
+
     hidden_size = 100
     num_classes = 10
 
@@ -26,21 +27,17 @@ def main():
     ])
 
     # Load the CIFAR-10 training dataset
-    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
     # Load the CIFAR-10 validation dataset
-    validation_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    
+    validation_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
     # Create model architecture
     network = BaseModel(hidden_size, num_classes)
 
     # Perform Fed-Ensemble in federated learning
-    ensemble_models = fed_ensemble(NUM_CLIENTS, NUM_MODELS, NUM_SELECTED_CLIENTS, NUM_AGES, BaseModel, train_dataset, 
-                                   validation_dataset, batch_size, num_epochs, learning_rate)
-
-    # Save the ensemble model
-    for model in ensemble_models:
-      torch.save(model.state_dict(), f"model_{i}.pth")
+    train(NUM_CLIENTS, NUM_MODELS, NUM_SELECTED_CLIENTS, NUM_AGES, BaseModel, train_dataset,
+                                   validation_dataset, batch_size, num_epochs, learning_rate, NUM_STRATA)
 
 if __name__ == '__main__':
     main()
